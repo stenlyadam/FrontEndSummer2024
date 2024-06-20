ee8e850b0616ca294d038c392800f1ced2251a2f
stenlyadam <stenly.adam@unklab.ac.id>
2024-06-20 09:57:09 +0800

    testing git pull

--- pertemuan-14/index.js
+++ pertemuan-14/index.js
@@ -1,4 +1,5 @@
 //Working on Remote Repository
 console.log("Hello Git and Github");
 console.log("Coba lagi");
-console.log("Ini diubah langsung di github")
+console.log("Ini diubah langsung di github");
+console.log("Test git pull");
